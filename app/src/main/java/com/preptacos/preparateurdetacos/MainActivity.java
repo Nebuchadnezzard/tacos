65922713fc0bbae88cf0ee04878a1becd40e6bd9
PIERRE COUDERC <couderc@iut.univ-paris5.fr>
2018-02-08 15:29:14 +0100

    init commit

---
+++ app/src/main/java/com/preptacos/preparateurdetacos/MainActivity.java
@@ -0,0 +1,23 @@
+package com.preptacos.preparateurdetacos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        //EditText ingredient = new EditText(MainActivity.this);
+
+        //Button btn = (Button) findViewById(R.id.button).addView(ingredient);
+    }
+}
